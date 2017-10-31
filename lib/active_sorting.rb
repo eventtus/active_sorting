b75dec1f9b310055a68469e3a5327fd62b26b606
Mohamed Elfiky <mohamed.elfikyy@yahoo.com>
2017-10-31 13:26:23 +0200

    add exceptions module and define new custom exception to be rescued

--- lib/active_sorting.rb
+++ lib/active_sorting.rb
@@ -1,4 +1,5 @@
 require 'rails/engine'
 require 'active_record'
 require 'active_sorting/version'
+require 'active_sorting/exceptions'
 require 'active_sorting/engine'
---
+++ lib/active_sorting/exceptions.rb
@@ -0,0 +1,10 @@
+module ActiveSorting
+  module Exceptions
+    class RecordsNotFound < StandardError
+    end
+
+    class InvalidListSize < StandardError
+    end
+  end
+end
+
